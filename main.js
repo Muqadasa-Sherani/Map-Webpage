e358286e1ee2633152afecbaefdb1ed6c6ae315a
Muqadasa-Sherani <kawsar.moqadsa@gmail.com>
2022-07-19 17:01:15 +0300

    jspanel popup on double click row. Edit and Delete buttons on popup panel.
    On double click on any row of the datatable there is a jspanel popup that will have the data on input boxes and there will be edit and delete buttons.
    Delete fetch api and deletion of row.

--- main.js
+++ main.js
@@ -110,11 +110,11 @@ function init(){
               height: "30%"
           },
           content: ' <div class="form" >'+
-          '<label for="x">X: </label>'+'<input type="text" id="x" value="'+X+'" readonly/>' + 
+          '<label class= "formLabel" for="x">X: </label>'+'<input type="text" id="x" value="'+X+'" readonly/>' + 
           '<br><br>'+
-          '<label for="y">Y: </label>'+'<input type="text" id="y" value="'+Y+'" readonly/>' + 
+          '<label class= "formLabel" for="y">Y: </label>'+'<input type="text" id="y" value="'+Y+'" readonly/>' + 
           '<br><br>'+
-          '<label for="name">Name: </label>'+ '<input type="text" id="name" placeholder="Name of the place.."/>'+
+          '<label class= "formLabel" for="name">Name: </label>'+ '<input type="text" id="name" placeholder="Name of the place.."/>'+
           '<br><br>'+
           '<button id="addButton">Add</button>'+
           '</div>'
@@ -169,7 +169,7 @@ function init(){
 
     let resultString = []; //result or response array is global.
     document.getElementById("queryLocationButton").addEventListener('click', function(){ //we fetch all the data from the databse and show on the data table.
-
+      
       fetch("https://localhost:7031/Locationt", { //all the coordinates from the database are recieved
         method: 'GET',
         headers: {
@@ -181,38 +181,148 @@ function init(){
 
         resultString = response;
           var panel = jsPanel.create({      //jspanel is pop-upped.
-            id: "tablePanel",
             headerTitle: 'Location Query',
             theme: '#00205c',
             contentSize: {
                 width: "800px",
                 height: "520px"
             },
-            content: ' <table id="myTable"></table>'
+            content: ' <table id="myTable"><thead><tr><th>Location ID</th><th>Location Name</th><th>X-coord</th><th>Y-coord</th></tr></thead></tbody></tbody></table>'
         }); //jspanel is created
 
-       console.log(resultString);
-
         $(document).ready( function () {
-          $('#myTable').DataTable({
+          var table = $('#myTable').DataTable({
             data: resultString,
             columns: [
               { data: 'id' },
               { data: 'name' },
               { data: 'x' },
               { data: 'y' },
-              /*{ title: 'id' },
-              { title: 'name' },
-              { title: 'x' },
-              { title: 'y' }*/
             ],
+            /*columnDefs: [ //for buttons on each row. Edit and Delete buttons.
+              {
+                  targets: -1,
+                  defaultContent: '<button>Edit</button>',
+              },
+              {
+                targets: -1,
+                defaultContent: '<button>Delete</button>',
+              },
+            ],*/
           });
-        });
+
+          //on double clicking a row we pop-up another jspanel with the name and coordinates in input boxes
+          $('#myTable tbody').on('dblclick', 'tr', function () {
+            var data = table.row(this).data();
+
+            //jspanel
+            jsPanel.create({
+              headerTitle: 'Location Data',
+              theme: '#00205c',
+              contentSize: {
+                  width: "30%",
+                  height: "30%"
+              },
+              content: ' <div class="form" >'+
+                      '<Label class="tabelFormLabel" >Name: </Label><input type="text" name="name" id="name" value="'+table.row(this).data().name+'">'+
+                      '<br><br>'+
+                      '<Label class="tabelFormLabel" >X-coordinates: </Label><input type="number" name="x" id="x"value="'+table.row(this).data().x+'">'+
+                      '<br><br>'+
+                      '<Label class="tabelFormLabel" >Y-coordinates: </Label><input type="number" name="y" id="y"value="'+table.row(this).data().y+'">'+
+                      '<br><br>'+
+                      '<button id="editButton">Edit</button>'+
+                      '<button id="deleteButton">Delete</button>'+
+                      '</div>'
+            });//jspanel finish.
+
+            //////////////////////////////////////////////////////this onclick function does not work.
+            /*$('#myTable').on('click', '#deleteButton', function () {
+              jsPanel.create({
+                headerTitle: 'Location Delete',
+                theme: '#00205c',
+                content: '<h2>Are you sure you want to delete location?</h2>'+
+                '<button id="yes">Yes, I am sure.</button>'+
+                '<button id="no">No, I am not sure.</button>'
+              });
+          });*/
+
+          document.getElementById("deleteButton").addEventListener('click', function(){ //delete button on click
+            //a jspanel to validate the deletion of a location.
+             jsPanel.create({
+              id: "deleteButtonPanel",
+              headerTitle: 'Location Delete',
+              theme: '#00205c',
+              content: '<h2>Are you sure you want to delete location?</h2>'+
+              '<button id="yes">Yes, I am sure.</button>'+
+              '<button id="no">No, I am not sure.</button>'
+            });
+
+            //if yes is clicked.
+            document.getElementById("deleteButton").addEventListener('click', function(){ //location is deleted.
+              //Delete fetch
+              fetch("https://localhost:7031/Locationt", {
+                method: 'DELETE',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+              })
+              .then((response) => response.json())
+              .then((response) => {
+                if(!response.status){
+                  //warning, not found
+                  toastr.warning('The location that you are deleting does not exist.', 'Warning');
+                }
+                else{
+                  toastr.success('Location is deleted successfully.', 'Success');
+                  table.row(this).delete(); //the row clicked on is deleted from the datattable.
+                }
+                
+              })
+              .catch((error) => {
+                //error, unexpected error happened.
+                toastr.error('Unexpected error occurred.', 'Error');
+              });
+              deleteButtonPanel.close(); //after the button is clicked the panel is closed.
+            });
+
+          });
+
+          ////////////////////////////////////////////////////////////
+
+          });//double click finish.
+
+          /////////////////////////////////////////////////////////////this does not work, if the onclick for delete is here.
+          /*$('#myTable').on('click', '#deleteButton', function () {
+            jsPanel.create({
+              headerTitle: 'Location Delete',
+              theme: '#00205c',
+              content: '<h2>Are you sure you want to delete location?</h2>'+
+              '<button id="yes">Yes, I am sure.</button>'+
+              '<button id="no">No, I am not sure.</button>'
+            });
+        });*/
+        ////////////////////////////////////////////////////////////////
+
+        });//datatable finish
+
       })
       .catch((error) => {
         //error, unexpected error happened.
         toastr.error('Unexpected error occurred.', 'Error');
       });// GET fetch finished
+
+      ///////////////////////////////////////////////////this does not work, if the onclick for delete is here.
+      /*document.getElementById("deleteButton").addEventListener('click', function(){ //delete button on click
+        jsPanel.create({
+          headerTitle: 'Location Delete',
+          theme: '#00205c',
+          content: '<h2>Are you sure you want to delete location?</h2>'+
+          '<button id="yes">Yes, I am sure.</button>'+
+          '<button id="no">No, I am not sure.</button>'
+        });
+      });*/
+      ////////////////////////////////////////////////
 
     });
 
@@ -230,5 +340,12 @@ function init(){
   if(negative first)else(positive)
   CORS: security port. modify/limit with, we only want post and update but not delete or get etc
   programs.cs file--> write after addswager();  --> builder.Services.Addcors(x)  --->after app.UseCors("my-policy");
+
+  delete from table ---> delete from point map
+  modify interaction:
+  edit button--> panel button "modify"-->onclick--->drag on map is active.
+
+  august start email cc daniel ask if there is any empty place.
+
 
   */
